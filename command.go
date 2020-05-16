175094dad895407917b1f6e2cbe104ea6b452a6e
Derek <d.halman@gmail.com>
2020-05-15 20:29:02 -0700

    Remove docs, move to vroomy

--- command.go
+++ command.go
@@ -14,8 +14,6 @@ func commandFromArgs() (cmd *flag.Command, err error) {
 	p.AddHandler("", help, "Manages vroomy packages.\n  To learn more, run `vpm help` or `vpm help <command>`")
 	p.AddHandler("help", help, "Prints available commands and flags.\n  Use `vpm help <command>` to get more specific info.")
 
-	p.AddHandler("doc", doc, "Outputs postman docs for specified plugin(s).\n  Accepts filtered trailing args to target specific plugins.\n  Use `vpm doc` for all plugins, or `vpm doc <plugin> <plugin>`")
-
 	p.AddHandler("update", update, "Loads specified version or latest channel from config and builds plugin(s).\n  Accepts filtered trailing args to target specific plugins.\n  Use `vpm update` for all plugins, or `vpm update <plugin> <plugin>`")
 	p.AddHandler("build", build, "Builds the currently checked out version of plugin(s).\n  Accepts filtered trailing args to target specific plugins.\n  Use `vpm build` for all plugins, or `vpm build <plugin> <plugin>`")
 
@@ -44,18 +42,6 @@ func help(cmd *flag.Command) (err error) {
 	}
 
 	fmt.Println(cmd.Help())
-	return
-}
-
-func doc(cmd *flag.Command) (err error) {
-	_, msg := commandParams(cmd)
-	out.Notificationf("Documenting %s...", msg)
-
-	/*
-		if err := v.documentPlugins(args...); err != nil {
-			handleError(err)
-		}
-	*/
 	return
 }
 
