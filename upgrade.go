cd5f6f1cd2937bd311c19c50a003841fd0bceea0
Derek <d.halman@gmail.com>
2020-05-17 17:37:15 -0700

    Updated messaging
    gomu: Deploy local changes

--- upgrade.go
+++ upgrade.go
@@ -49,7 +49,7 @@ func upgrade(cmd *flag.Command) (err error) {
 		version = cmd.StringFrom("-branch")
 	}
 
-	lib.File.Output("Checking vpm installation...")
+	out.Notification("Checking vpm installation...")
 	currentVersion, _ = lib.File.CmdOutput("vpm", "version")
 	originalBranch, _ = lib.File.CurrentBranch()
 	hasChanges = lib.File.HasChanges()
@@ -62,7 +62,7 @@ func upgrade(cmd *flag.Command) (err error) {
 		if len(currentVersion) > 0 && currentVersion == version {
 			if output, err = lib.File.CmdOutput("git", "rev-list", "-n", "1", version); err != nil {
 				// No tag set. skip tag
-				lib.File.Output("No revision history. Skipping tag.")
+				out.Notification("No revision history. Skipping tag.")
 				return
 			}
 
@@ -70,7 +70,7 @@ func upgrade(cmd *flag.Command) (err error) {
 
 			if output, err = lib.File.CmdOutput("git", "rev-parse", "HEAD"); err != nil {
 				// No tag set. skip tag
-				lib.File.Output("No revision head. Skipping tag.")
+				out.Notification("No revision head. Skipping tag.")
 				return
 			}
 
@@ -78,13 +78,13 @@ func upgrade(cmd *flag.Command) (err error) {
 
 			if tagCommit == headCommit {
 				if hasChanges {
-					lib.File.Output("There appears to be local changes...")
+					out.Notification("There appears to be local changes...")
 				} else {
-					lib.File.Output("Version is up to date!")
+					out.Successf("%s is up to date!", version)
 					return
 				}
 			} else {
-				lib.File.Output("There appears to be an untagged commit...")
+				out.Notification("There appears to be an untagged commit...")
 			}
 		}
 	}
@@ -95,23 +95,23 @@ func upgrade(cmd *flag.Command) (err error) {
 		msg = "latest"
 	}
 
-	lib.File.Output("Upgrading Installation from " + currentVersion + " to " + version + "...")
+	out.Notification("Upgrading Installation from " + currentVersion + " to " + version + "...")
 
 	if len(version) > 0 {
-		lib.File.Output("Setting local vpm repo to: " + version + "...")
+		out.Notification("Setting local vpm repo to: " + version + "...")
 
 		if err = lib.File.CheckoutBranch(version); err != nil {
-			lib.File.Output("Failed to checkout " + version + " :(")
+			out.Notification("Failed to checkout " + version + " :(")
 			return
 		}
 
 		lib.File.Pull()
 
 	} else {
-		lib.File.Output("Updating source...")
+		out.Notification("Updating source...")
 
 		if lib.File.Pull() != nil {
-			lib.File.Output("Failed to update source :(")
+			out.Notification("Failed to update source :(")
 		}
 	}
 
@@ -124,7 +124,7 @@ func upgrade(cmd *flag.Command) (err error) {
 
 			if err != nil {
 				// No tag set. skip tag
-				lib.File.Output("No revision history. Skipping tag.")
+				out.Notification("No revision history. Skipping tag.")
 
 				if len(originalBranch) > 0 {
 					lib.File.CheckoutBranch(originalBranch)
@@ -139,7 +139,7 @@ func upgrade(cmd *flag.Command) (err error) {
 			output, err = lib.File.CmdOutput("git", "rev-parse", "HEAD")
 
 			if err != nil {
-				lib.File.Output("No revision head. Cannot checkout version.")
+				out.Notification("No revision head. Cannot checkout version.")
 
 				if len(originalBranch) > 0 {
 					lib.File.CheckoutBranch(originalBranch)
@@ -158,7 +158,7 @@ func upgrade(cmd *flag.Command) (err error) {
 
 	if currentVersion == version && tagCommit == headCommit {
 		if !hasChanges {
-			lib.File.Output("Version is up to date!")
+			out.Successf("%s is up to date!", version)
 
 			if len(originalBranch) > 0 {
 				lib.File.CheckoutBranch(originalBranch)
@@ -168,13 +168,13 @@ func upgrade(cmd *flag.Command) (err error) {
 		}
 	}
 
-	lib.File.Output("Installing " + version + "...")
+	out.Notification("Installing " + version + "...")
 
 	if err = lib.File.RunCmd("./bin/install", version); err != nil {
 		// Try again with permissions
 		err = nil
 		if err = lib.File.RunCmd("sudo", "./bin/install", version); err != nil {
-			lib.File.Output("Failed to install :(")
+			out.Notification("Failed to install :(")
 
 			if len(originalBranch) > 0 {
 				lib.File.CheckoutBranch(originalBranch)
@@ -186,7 +186,7 @@ func upgrade(cmd *flag.Command) (err error) {
 		lib.File.RunCmd("sudo", "chown", "-R", usr.Name, path.Join(usr.HomeDir, "go", "pkg"))
 	}
 
-	lib.File.Output("Installed Successfully!")
+	out.Notification("Installed Successfully!")
 
 	if len(originalBranch) > 0 {
 		lib.File.CheckoutBranch(originalBranch)
